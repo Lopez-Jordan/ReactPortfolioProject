4cb44f964643361175ef856cc5f8fdf0c019171a
Jordan Lopez <jordanlopezemail@gmail.com>
2023-08-30 23:54:10 -0700

    the contact me form is now done

--- React-Portfolio-Project/src/pages/Contact.jsx
+++ React-Portfolio-Project/src/pages/Contact.jsx
@@ -1,15 +1,77 @@
+import { useState } from 'react';
+
 export default function Contact() {
-    return (
-      <div>
-        <h1>Contact Me Page</h1>
-        <p>
-          Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-          Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-          mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-          lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-          imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-          in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-        </p>
-      </div>
-    );
-  }
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    alert(`Success, ${name}! This form doesn't work though :/`);
+    setName('');
+    setEmail('');
+    setMessage('');
+  };
+
+  const inputStyles = {
+    padding: '10px',
+    marginBottom: '15px',
+    border: '1px solid #ccc',
+    borderRadius: '5px',
+    width: '100%',
+  };
+
+  const buttonStyles = {
+    padding: '10px 20px',
+    backgroundColor: '#007bff',
+    color: 'white',
+    border: 'none',
+    borderRadius: '5px',
+    cursor: 'pointer',
+  };
+
+  return (
+    <div style={{ textAlign: 'center', margin: '50px auto', padding: '20px' }}>
+      <h1>Contact Me!</h1>
+      <h4>I would love to answer any questions about my work</h4>
+      <form
+        style={{ display: 'flex', flexDirection: 'column', maxWidth: '400px', margin: '0 auto' }}
+        onSubmit={handleFormSubmit}
+      >
+        <input
+          style={inputStyles}
+          value={name}
+          name="name"
+          onChange={(e) => setName(e.target.value)}
+          type="text"
+          placeholder="Name"
+        />
+        <input
+          style={inputStyles}
+          value={email}
+          name="email"
+          onChange={(e) => setEmail(e.target.value)}
+          type="text"
+          placeholder="JohnDo@gmail.com"
+        />
+        <textarea
+          style={inputStyles}
+          value={message}
+          name="message"
+          onChange={(e) => setMessage(e.target.value)}
+          placeholder="Message goes here"
+        />
+        <button style={buttonStyles} type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
+
+
+
+
+
+
+
