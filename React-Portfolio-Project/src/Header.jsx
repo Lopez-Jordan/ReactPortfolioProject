8b462f3465def65465920f59ed487f825cc41164
Jordan Lopez <jordanlopezemail@gmail.com>
2023-08-30 22:58:52 -0700

    pseudocode and setup for app.jsx and main.jsx mostly done

---
+++ React-Portfolio-Project/src/Header.jsx
@@ -0,0 +1,12 @@
+import Navbar from './components/Navbar';
+
+export default function Header(){
+    return (
+        <>
+            <div>
+                Header With navbar!
+            </div>
+            <Navbar/>
+        </>
+    );
+}
--- React-Portfolio-Project/src/main.jsx
+++ React-Portfolio-Project/src/main.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import App from './App'
+
+import './assets/index.css'  // CSS FILE
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import Contact from './pages/Contact';
+import AboutMe from './pages/AboutMe';
+import Projects from './pages/Projects';    
+
+const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <App />,
+      children: [
+        {
+          index: true,
+          element: <AboutMe/>,
+        },
+        {
+          path: '/Projects',
+          element: <Projects/>,
+        },
+        {
+          path: '/Contact',
+          element: <Contact/>,
+        },
+      ],
+    },
+  ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  );
