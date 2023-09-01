36a4d7a4e88c6f8974ee6709541a3c2e5e24cf02
Jordan Lopez <jordanlopezemail@gmail.com>
2023-08-31 23:19:43 -0700

    icons are now done

--- React-Portfolio-Project/src/components/Footer.jsx
+++ React-Portfolio-Project/src/components/Footer.jsx
@@ -1,14 +1,55 @@
-export default function Footer (){
-    const footerStyles = {
-        bottom: '0',
-        position: 'fixed',
-        padding: '40px',
-        width: '100%',
-    }   
+import { FaInstagram, FaLinkedinIn, FaGithub } from 'react-icons/fa';
+import { useState } from 'react';
 
-    return (
-        <div style={footerStyles}>
-            Footer
-        </div>
-    );
+export default function Footer() {
+
+  const iconStyle = {
+    fontSize: '50px',
+    color: '#8CBCFF',
+    transition: 'color 0.5s ease',
+    margin: '0 70px', // Margin on each side
+    cursor: 'pointer', // Show cursor pointer on hover
+  };
+
+  const iconHoverStyle = {
+    color: 'white', // Color on hover
+  };
+
+  const footerStyles = {
+    bottom: '0',
+    position: 'fixed',
+    display: 'flex',
+    justifyContent: 'center', // Center icons horizontally
+    alignItems: 'center', // Center icons vertically
+    padding: '40px',
+    width: '100%',
+  };
+  const [hoveredIcon, setHoveredIcon] = useState(null);
+
+  return (
+    <div style={footerStyles}>
+        <a href="https://www.instagram.com/jordan___lopez/">
+            <FaInstagram
+                style={hoveredIcon === 'Instagram' ? { ...iconStyle, ...iconHoverStyle } : iconStyle}
+                onMouseEnter={() => setHoveredIcon('Instagram')}
+                onMouseLeave={() => setHoveredIcon(null)}
+            />
+        </a>
+
+        <a href="https://www.linkedin.com/in/jordanlopez123/">
+            <FaLinkedinIn
+                style={hoveredIcon === 'LinkedIn' ? { ...iconStyle, ...iconHoverStyle } : iconStyle}
+                onMouseEnter={() => setHoveredIcon('LinkedIn')}
+                onMouseLeave={() => setHoveredIcon(null)}
+            />
+        </a>
+        <a href="https://github.com/Lopez-Jordan">
+            <FaGithub
+                style={hoveredIcon === 'Github' ? { ...iconStyle, ...iconHoverStyle } : iconStyle}
+                onMouseEnter={() => setHoveredIcon('Github')}
+                onMouseLeave={() => setHoveredIcon(null)}
+            />
+        </a>
+    </div>
+  );
 }
